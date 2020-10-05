b643ff39b862367b1e88e2d3b5c01e51c3e6e876
Rohit Wadke <rohit.m.wadke@gmail.com>
2020-10-06 00:49:44 +0530

    Convert Binary to Decimal Number
    Convert an integer number in Binary to Decimal Number.

---
+++ C codes/BinaryToDecimal.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	long int i,n,x=0,a;
+	printf("Enter any binary number: ");
+	scanf("%ld",&n);
+	printf("\nThe decimal conversion of %ld is ",n);
+	for(i=0;n!=0;++i)
+	{
+		a=n%10;
+		x=(a)*(pow(2,i))+x;
+		n=n/10;
+	}
+	printf("%ld",x);
+	return 0;	
+}
