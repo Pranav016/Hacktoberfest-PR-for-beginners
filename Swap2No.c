1b34458191b63ac6fb8e39949ec8d917b4fe03dd
Krishan Shamod <59104070+krishanshamod@users.noreply.github.com>
2020-10-28 00:39:06 +0530

    Update Swap2No.c

--- Swap2No.c
+++ Swap2No.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main() {
-      double first, second, temp;
+      int first, second, temp;
       printf("Enter first number: ");
-      scanf("%lf", &first);
+      scanf("%d", &first);
       printf("Enter second number: ");
-      scanf("%lf", &second);
+      scanf("%d", &second);
 
       // Value of first is assigned to temp
       temp = first;
@@ -15,7 +15,7 @@ int main() {
       // Value of temp (initial value of first) is assigned to second
       second = temp;
 
-      printf("\nAfter swapping, firstNumber = %.2lf\n", first);
-      printf("After swapping, secondNumber = %.2lf", second);
+      printf("\nAfter swapping, firstNumber = %d\n", first);
+      printf("After swapping, secondNumber = %d", second);
       return 0;
 }
